14053c77c9ff1d48d1af2c706a85cdc363cc1584
silacamp873 <silacamp873@SDS-L-9982Y93>
2024-05-22 13:47:59 -0400

    Add project files.

---
+++ Summative Project Space Race/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Summative_Project_Space_Race
+{
+    partial class SpaceRacer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.timeLabel.Font = new System.Drawing.Font("Bauhaus 93", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(249, 9);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(174, 32);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "Space Racer";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Cascadia Mono", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(365, 485);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(58, 56);
+            this.p2ScoreLabel.TabIndex = 2;
+            this.p2ScoreLabel.Text = "0";
+            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Cascadia Mono", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(234, 485);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(58, 56);
+            this.p1ScoreLabel.TabIndex = 3;
+            this.p1ScoreLabel.Text = "0";
+            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winLabel
+            // 
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(224, 206);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(227, 102);
+            this.winLabel.TabIndex = 4;
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(224, 292);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(227, 102);
+            this.titleLabel.TabIndex = 5;
+            this.titleLabel.Text = "PRESS SPACE TO START\r\nPRESS ESC TO EXIT\r\n";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SpaceRacer
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::Summative_Project_Space_Race.Properties.Resources.ezgif_2_df57a1687b;
+            this.ClientSize = new System.Drawing.Size(650, 550);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.timeLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SpaceRacer";
+            this.Text = "Space Racer";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.SpaceRacer_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SpaceRacer_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SpaceRacer_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ Summative Project Space Race/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Summative_Project_Space_Race
+{
+    public partial class SpaceRacer : Form
+    {
+        //Gloabal variables
+        //Player variables
+        Rectangle player1 = new Rectangle(163, 505, 20, 30);
+        Rectangle player2 = new Rectangle(487, 505, 20, 30);
+
+        int playerSpeed = 10;
+        int meteorSize = 0;
+
+        //Lists
+        List<Rectangle> meteorList = new List<Rectangle>();
+        List<int> meteorSizes = new List<int>();
+        List<int> meteorSpeeds = new List<int>();
+
+        int player1Score = 0;
+        int player2Score = 0;
+        int time = 1000;
+
+        Random randGen = new Random();
+        Random randSize = new Random();
+        Random randomSpeeds = new Random();
+
+        int randValue = 0;
+
+        bool upPressed = false;
+        bool downPressed = false;
+        bool wPressed = false;
+        bool sPressed = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.Blue);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        public SpaceRacer()
+        {
+            InitializeComponent();
+        }
+
+        private void SpaceRacer_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upPressed = true;
+                    break;
+                case Keys.Down:
+                    downPressed = true;
+                    break;
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+                case Keys.Space:
+                    gameTimer.Enabled = true;
+                    InitializeGame();
+                    break;
+                case Keys.Escape:
+                    if (gameTimer.Enabled == false)
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void SpaceRacer_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upPressed = false;
+                    break;
+                case Keys.Down:
+                    downPressed = false;
+                    break;
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //Making a game timer
+            time--;
+            timeLabel.Text = $"{time}";
+
+            MeteorMovement();
+
+            MeteorRandomGeneration();
+
+            OutOfBounds();
+
+            ScoringMethod();
+
+            PlayerCollisions();
+
+            WinMethod();
+
+            PlayerMovement();
+
+            Refresh();
+        }
+
+        public void InitializeGame()
+        {
+            winLabel.Visible = false;
+            titleLabel.Visible = false;
+            p2ScoreLabel.Text = "0";
+            p1ScoreLabel.Text = "0";
+
+            meteorList.Clear();
+            meteorSizes.Clear();
+            meteorSpeeds.Clear();
+
+            time = 1000;
+            player1Score = 0;
+            player2Score = 0;
+            player1.Y = 505;
+            player2.Y = 505;
+        }
+        public void MeteorMovement()
+        {
+            //For generating sideways moving meteors continuously
+            for (int i = 0; i < meteorList.Count(); i++)
+            {
+                int x = meteorList[i].X + meteorSpeeds[i];
+                meteorList[i] = new Rectangle(x, meteorList[i].Y, meteorSizes[i], meteorSizes[i]);
+            }
+        }
+        public void MeteorRandomGeneration()
+        {
+            randValue = randGen.Next(1, 100);
+
+            //Making meteors and their sizes and speeds random
+            if (randValue <= 15)
+            {
+                randValue = randGen.Next(30, this.Height);
+                Rectangle meteor = new Rectangle(0, randValue, meteorSize, meteorSize);
+                meteorList.Add(meteor);
+
+                meteorSizes.Add(randSize.Next(3, 10));
+                meteorSpeeds.Add(randomSpeeds.Next(4, 17));
+            }
+
+            if (randValue > 15 && randValue <= 30)
+            {
+                randValue = randGen.Next(30, this.Height);
+                Rectangle meteor = new Rectangle(650, randValue, meteorSize, meteorSize);
+                meteorList.Add(meteor);
+
+                meteorSizes.Add(randSize.Next(3, 10));
+                meteorSpeeds.Add(randomSpeeds.Next(-17, -4));
+            }
+        }
+        public void OutOfBounds()
+        {
+            //Removing meteor if it goes out of bounds
+            for (int i = 0; i < meteorList.Count(); i++)
+            {
+                if (meteorList[i].X > 650 || meteorList[i].Y > 505)
+                {
+                    meteorList.RemoveAt(i);
+                    meteorSpeeds.RemoveAt(i);
+                    meteorSizes.RemoveAt(i);
+                }
+            }
+        }
+        public void ScoringMethod()
+        {
+            //Making the players get a point when they reach the end and then bringing them back down
+            if (player1.Y < 1)
+            {
+                SoundPlayer pointPlayer = new SoundPlayer(Properties.Resources.point);
+                pointPlayer.Play();
+                
+                player1.Y = 505;
+                player1Score++;
+                p1ScoreLabel.Text = $"{player1Score}";
+            }
+
+            if (player2.Y < 1)
+            {
+                SoundPlayer pointPlayer = new SoundPlayer(Properties.Resources.point);
+                pointPlayer.Play();
+
+                player2.Y = 505;
+                player2Score++;
+                p2ScoreLabel.Text = $"{player2Score}";
+            }
+        }
+        public void PlayerCollisions()
+        {
+            //Making it so that if player 1 or 2 intersects with a meteor they go back down
+            for (int i = 0; i < meteorList.Count(); i++)
+            {
+                if (meteorList[i].IntersectsWith(player1))
+                {
+                    player1.Y = 505;
+                    meteorList.RemoveAt(i);
+                    meteorSpeeds.RemoveAt(i);
+                    meteorSizes.RemoveAt(i);
+                }
+
+                if (meteorList[i].IntersectsWith(player2))
+                {
+                    player2.Y = 505;
+                    meteorList.RemoveAt(i);
+                    meteorSpeeds.RemoveAt(i);
+                    meteorSizes.RemoveAt(i);
+                }
+            }
+        }
+        public void WinMethod()
+        {
+            //Deciding who wins at the end of the game
+            if (time == 0)
+            {
+                gameTimer.Stop();
+                SoundPlayer gongPLayer = new SoundPlayer(Properties.Resources.gong);
+                gongPLayer.Play();
+
+                winLabel.Visible = true;
+                titleLabel.Visible = true;
+
+                winLabel.Text = "PRESS SPACE TO RESTART \n\n PRESS ESC TO EXIT";
+
+                if (player1Score > player2Score)
+                {
+                    winLabel.Text = "PLAYER 1 WINS";
+                }
+                else if (player2Score > player1Score)
+                {
+                    winLabel.Text = "PLAYER 2 WINS";
+                }
+                else
+                {
+                    winLabel.Text = "TIE GAME";
+                }
+            }
+        }
+        public void PlayerMovement()
+        {
+            //Player movement controls
+            if (upPressed == true && player2.Y > 0)
+            {
+                player2.Y = player2.Y - playerSpeed;
+            }
+
+            if (downPressed == true && player2.Y < 500)
+            {
+                player2.Y = player2.Y + playerSpeed;
+            }
+
+            if (wPressed == true && player1.Y > 0)
+            {
+                player1.Y = player1.Y - playerSpeed;
+            }
+
+            if (sPressed == true && player1.Y < 500)
+            {
+                player1.Y = player1.Y + playerSpeed;
+            }
+        }
+        private void SpaceRacer_Paint(object sender, PaintEventArgs e)
+        {
+            //Drawing players and meteors
+            if (gameTimer.Enabled == true)
+            {
+                e.Graphics.FillRectangle(blueBrush, player1);
+                e.Graphics.FillRectangle(redBrush, player2);
+
+                for (int i = 0; i < meteorList.Count(); i++)
+                {
+                    e.Graphics.FillEllipse(whiteBrush, meteorList[i]);
+                }
+            }
+        }
+    }
+}
